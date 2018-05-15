f406cc0d8d749381607f693b1d49e13824295a3b
Dawid Matusiewicz <d.matusiewicz@kainos.com>
2018-05-15 14:01:55 +0200

    Removing a module - procedure

--- entrypoints/package-modules.sh
+++ entrypoints/package-modules.sh
@@ -4,4 +4,8 @@
 . /etc/init.d/common.sh
 
 cd ${BUILD_DIR}/puppet/main
-librarian-puppet package
+echo -e "${SET_GREEN}[Shell] Installing modules${RESET}"
+librarian-puppet install --clean --destructive --verbose
+
+echo -e "${SET_GREEN}[Shell] Packaging modules${RESET}"
+librarian-puppet package --clean --destructive --verbose
